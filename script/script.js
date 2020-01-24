ef1167a12da1411d801d48d5d998167050898f54
Ivan Chutkan <alexandrbozhkov1977@gmail.com>
2020-01-24 16:26:16 +0200

    Lesson_02 easy is done

--- script/script.js
+++ script/script.js
@@ -1,38 +1,22 @@
-// STRING///////////////////////////////////////////
+let money = 150000;
+let income = 'шантаж, вымогательство, разбой';
+let addExpenses = 'секс, наркотики, рок-н-ролл';
+let deposit = 4 > 3;
+// console.log(deposit);
+let mission = 1000000;
+let period = 12;
 
-// let myStr = '\t "Hello" my \n friends';
-// console.log(myStr);
-// let myStr2 = '\t Hello "my" \n \t friends';
-// console.log(myStr2);
-// let myStr3 = 'Hello my \n \t "friends"';
-// console.log(myStr3);
+console.log(typeof money, typeof income, typeof deposit);
 
+console.log('Длина строки составляет ' + 
+addExpenses.length + ' символов');
 
-//МЕТОДЫ И СВОЙСТВА//////////////////////////////////////
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель - заработать ' + mission + ' долларов');
 
-let str = 'Hello My Friends!';
-console.log('В строке ' + str.length + ' символов');
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-
-// console.log(str.charAt(0));
-// console.log(str.charAt(16));
-
-console.log(str[16]);
-
-console.log(str.substring(6)); // с 6-го символа
-console.log(str.substring(9, 14)); // с 9-го по 14-й
-console.log(str.slice(11)); // с 11-го символа до конца
-console.log(str.slice(-10)); // 10 символов с конца
-console.log(str.substr(3, 8)); // с какого символа, сколько знаков
-
-console.log(str.indexOf('Friends')); // возвращает индекс первой буквы слова
-
-console.log(str.indexOf('d')); // индекс символа (ближайший)
-
-console.log(str.replace('Friends', 'Slaves')); // Меняет строку
-
-console.log(str.split(' '));
-
-let myStr = 'apple, kiwi, orange, cherry';
-console.log(myStr.split(', '));
+console.log(addExpenses.toUpperCase(), addExpenses.split(', '));
+console.log(addExpenses.split(''));
+let budgetDay;
+budgetDay = money;
+console.log('Предпочтительный доход - ' + budgetDay + 
+' рублей в месяц');
